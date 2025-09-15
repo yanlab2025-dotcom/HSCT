eca089f9a9beee73358480a927e9d801e72aeb51
yanlab2025-dotcom <yanlab2025@gmail.com>
2025-09-15 13:53:44 +0800

    Update and rename fig6C_E.R to fig6c_d.R

---
+++ Fig6/fig6c_d.R
@@ -0,0 +1,106 @@
+###########This is data for Fig c_d#####################
+rm(list=ls()) 
+library(readxl)
+library(dplyr)
+library(reshape2)
+library(ggplot2)
+library(ggpubr)
+library(gridExtra)
+library(grid)
+load("Fig6.RData")
+########################################################################################
+#effect size plot
+    aGVHD_brm$X <- factor(aGVHD_brm$X, levels = aGVHD_brm$X[order(aGVHD_brm$Estimate, decreasing = FALSE)])
+    Fig6C_1 <- ggplot(aGVHD_brm, aes(x = Estimate, y = X)) +
+      geom_point(size = 2) +  
+      geom_errorbarh(aes(xmin = l.95..CI, xmax = u.95..CI), 
+                     height = 0.2, color = "black", size = unit(0.2, "mm")) +  
+      geom_vline(xintercept = 0, linetype = "dashed", color = "gray") + 
+      coord_cartesian(clip = "off") +  
+      theme_classic() +
+      labs(x = "Effect Size (Estimate)", y = "") +
+      theme(
+        axis.ticks = element_line(size = unit(0.2, "mm")),
+        axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 10),
+        panel.border = element_rect(fill = NA, color = "black", size = unit(0.2, "mm")),  
+        plot.title = element_blank()
+      )
+    Fig6C_1
+########################################################################################
+    dt.response.boxplot_long$variable <- as.character(dt.response.boxplot_long$variable)
+    sorted_levels <- aGVHD_brm$X[order(aGVHD_brm$Estimate, decreasing = FALSE)]
+    dt.response.boxplot_long$variable <- factor(dt.response.boxplot_long$variable, levels = sorted_levels)
+    dt.response.boxplot_long$Target<-as.factor(dt.response.boxplot_long$Target)
+    dt.response.boxplot_long <- dt.response.boxplot_long %>%
+    mutate(rel_abundance_for_FC = if ("sqrtAbundance" %in% names(.)) sqrtAbundance else (sqrtAbundance)^2)
+    fc_tbl <- dt.response.boxplot_long %>%
+      group_by(variable, Target) %>%
+      summarize(med = median(rel_abundance_for_FC, na.rm = TRUE), .groups = "drop") %>%
+      tidyr::pivot_wider(names_from = Target, values_from = med, names_prefix = "grp_") %>%
+      mutate(grp_0 = ifelse(is.na(grp_0), 0, grp_0),
+           grp_1 = ifelse(is.na(grp_1), 0, grp_1)) %>%
+      mutate(FC = ifelse(grp_0 == 0 & grp_1 == 0, 1,
+                       ifelse(grp_0 == 0, Inf, grp_1 / grp_0)),
+           log2FC = ifelse(is.finite(FC) & FC > 0, log2(FC),
+                           NA_real_))
+  
+    fc_tbl<-as.data.frame(fc_tbl)
+    fc_tbl <- fc_tbl %>%
+      mutate(fc_centered = FC - 1)  
+    fc_tbl$variable <- factor(fc_tbl$variable, levels = sorted_levels)
+    Fig6D_1 <- ggplot(fc_tbl, aes(x = fc_centered, y = variable, fill = FC)) +
+      geom_segment(aes(x = fc_centered, xend = fc_centered, y = variable),
+                 linetype = "dotted", color = "gray30", linewidth = 0.4) +
+      geom_col(width = 0.6) +
+      geom_vline(xintercept = 0, linetype = "dashed", color = "gray30") +  
+      scale_x_continuous(
+      breaks = c(-1, -0.5, 0, 0.5, 1),
+      labels = c("0", "0.5", "1", "1.5", "2")   
+      ) +
+      scale_fill_gradientn(
+        colors = c("#51C3CC",
+                 "#bababa", 
+                 "#CC5800"),
+      limits = c(0, 2),         
+      breaks = c(0, 0.5, 1, 1.5, 2)
+      )+
+      theme_classic() +
+      labs(
+        x = "Fold Change", 
+        y = NULL, 
+        title = NULL
+      ) +
+      theme(
+       legend.position = "none",
+       axis.ticks = element_line(size = unit(0.2, "mm")),
+       axis.line = element_line(color = "black", size = unit(0.2, "mm")),
+       axis.text.y = element_blank(),
+       plot.title = element_text(hjust = 0.5, face = "bold")
+      )
+
+    Fig6D_1
+
+######################################################################################## 
+#combine all plots
+
+   Fig6C_1 <- Fig6C_1 + theme(plot.margin = unit(rep(0.1, 4), "cm"))
+   Fig6D_1 <- Fig6D_1 + theme(plot.margin = unit(rep(0, 4), "cm"))
+   g1 <- ggplotGrob(Fig6C_1)
+   g2 <- ggplotGrob(Fig6D_1)
+   max_height <- unit.pmax(g1$heights,  g2$heights)
+   g1$heights <- max_height
+   g2$heights <- max_height
+   Fig6C_2 <- Fig6C_2 + theme(plot.margin = unit(rep(0.1, 4), "cm"))
+   Fig6D_2 <- Fig6D_2 + theme(plot.margin = unit(rep(0, 4), "cm"))
+   combined_plot <- grid.arrange(g1, g2,
+                             ncol = 2,
+                             widths = c(4.1, 1.1),
+                             layout_matrix = rbind(c(1, 2)))
+
+
+
+
+
+
+           
