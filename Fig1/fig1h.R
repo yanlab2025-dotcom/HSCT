13bf185770c1b64a801c1d456d79ba1888c633b0
yanlab2025-dotcom <yanlab2025@gmail.com>
2025-09-18 21:20:14 +0800

    Add files via upload

---
+++ Fig1/fig1h.R
@@ -0,0 +1,55 @@
+###############This is data for fig 1h######
+rm(list = ls())
+library(readxl)
+library(dplyr)
+library(ggplot2)
+library(openxlsx) 
+library(ggpubr)
+library(survival)
+library(survminer)
+library(cowplot)
+load("fig1.RData")
+#######################################################################################
+#data input preparation
+    summarized_data$death <- as.numeric(ifelse(summarized_data$death == "Yes", 1, 0))
+    surv_object <- Surv(time = summarized_data$death_time, event = summarized_data$death)
+    km_fit <- survfit(surv_object ~ AUS_group, data = summarized_data)
+    
+#figure generation
+    km_plot <- ggsurvplot(
+      km_fit,
+      data = summarized_data,
+      pval = TRUE,  
+      pval.size = 3.5,
+      conf.int = TRUE, 
+      risk.table.title = "",
+      risk.table = TRUE,  
+      risk.table.fontsize = 3.5,         
+      legend.labs = c(  "High_Score","Low_Score"),  
+      legend.title = "AUS_death",  
+      xlab = "Time (days)",          
+      ylab = "Survival Probability", 
+      ggtheme = theme_classic(base_size = 12) +
+        theme(
+          axis.line.x = element_line(size = 0.33),
+          axis.title.x = element_blank(),
+          axis.text.x = element_text(size = 0),
+          axis.line.y = element_line(size = 0.33),
+          axis.ticks.length = unit(0.1, "cm")
+        ),
+      tables.theme = theme_classic(base_size = 1) +
+        theme(
+          axis.title.y = element_blank(),
+          axis.text.y = element_text(size = 10),
+          axis.title.x = element_text(size = 10),
+          axis.ticks = element_line(color = "black", size = 0.33),
+          axis.ticks.length = unit(0.1, "cm"),
+          axis.text.x = element_text(size = 10),
+          axis.line.x = element_line(size = 0.33),  
+          axis.line.y = element_line(size = 0.33)
+        ),
+      palette = c("#08306b","#93C6E1FF")   
+    )
+    fig1h <- plot_grid(km_plot$plot, km_plot$table, ncol = 1, align = "v", rel_heights = c(2, 1))
+
+    fig1h
---
+++ Fig1/fig1i.R
@@ -0,0 +1,45 @@
+###############This is data for fig 1i#######
+rm(list = ls())
+library(dplyr)
+library(ggplot2)
+load("fig1.Rdata")
+#####################################################################################
+#data input preparation
+    marker_names <- c(
+      "IL6" = "IL-6",
+      "CRP" = "C-Reactive Protein",
+      "IL10" = "IL-10",
+      "TNF-a" = "TNF-α",
+      "IL4" = "IL-4",
+      "IL2" = "IL-2",
+      "PCT" = "Procalcitonin"
+    )
+    df_all$Marker <- marker_names[df_all$Marker]
+    df_all$Marker <- factor(df_all$Marker, levels = rev(marker_names))
+    df_all$Variable <- factor(df_all$Variable, levels = c("AUS", "Shannon"))
+
+#figure generation
+#q_value < 0.05 ~ "*"
+#q_value < 0.25  ~ "."
+    fig1i <- ggplot(df_all, aes(x = Variable, y = Marker)) +
+      geom_tile(aes(fill = Correlation), color = "white", width = 1, height = 1) +
+      geom_text(aes(label = Signif), size = 5, color = "black") + 
+      scale_fill_gradient2(
+        low = "#1F8F99", mid = "#FFE6CC", high = "#994000", midpoint = 0
+      ) +
+      scale_size(range = c(3, 8)) +
+      theme_bw() +
+      theme(
+        axis.title = element_blank(),
+        axis.text.x = element_text(size = 10, color = "black"),
+        axis.text.y = element_text(size = 10, color = "black"),
+        panel.grid = element_blank(),
+        legend.key.size = unit(0.3, "cm"),
+        panel.border = element_rect(color = "black", fill = NA, size = 0.66),
+        axis.ticks.x = element_line(size = 0.33, colour = "black"),  
+        axis.ticks.y = element_line(size = 0.33, colour = "black"),   
+        axis.ticks.length = unit(0.1, "cm"),
+        legend.position = "right"
+      ) 
+
+    fig1i
